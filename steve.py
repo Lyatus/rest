a9ce584bb2e032aa3e04e2af715a8b86a69a7e0e
Lutopia <lutopia@hotmail.fr>
2024-09-13 00:38:04 +0200

    steve: Return filenames instead of paths

--- steve.py
+++ steve.py
@@ -9,11 +9,11 @@ GIT_REPO = "https://github.com/Lyatus/steve"
 router = APIRouter(prefix="/steve")
 
 @router.get("/configurations")
-def get_configurations():
+async def get_configurations():
   return list(map(lambda c: c.removesuffix('.steve.json'), os.listdir("program/steve/cfg")))
 
 @router.post("/generate")
-def generate(configuration: str):
+async def generate(configuration: str):
   config_path = f"program/steve/cfg/{configuration}.steve.json"
   if not os.path.exists(config_path):
     raise HTTPException(status_code=404, detail=f"Configuration '{configuration}' not found")
@@ -23,14 +23,16 @@ def generate(configuration: str):
   if not os.path.exists(steve_exe):
     steve_exe = "program/steve/bld/steve"
 
-  output_path = f"tmp/{str(uuid.uuid4())}"
-  error = subprocess.call(
-    [steve_exe,
-     "-mt",
-     "--random",
-     f"--out={output_path}",
-     f"--config={config_path}"])
+  filename = str(uuid.uuid4())
+  output_path = f"tmp/{filename}"
+  error = subprocess.call([
+    steve_exe,
+    "-mt",
+    "--random",
+    f"--out={output_path}",
+    f"--config={config_path}"
+  ])
   if error != 0:
     raise HTTPException(status_code=500, detail=f"Steve process returned {error}")
 
-  return [f"{output_path}.mid",f"{output_path}.txt"]
+  return [f"{filename}.mid",f"{filename}.txt"]
